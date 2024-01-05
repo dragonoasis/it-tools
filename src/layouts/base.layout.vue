180f2d387b16ffa4691f076a44a48d11b10ce9fa
dragonoasis <132386579+dragonoasis@users.noreply.github.com>
2024-01-04 23:11:03 -0800

    Update links

--- src/layouts/base.layout.vue
+++ src/layouts/base.layout.vue
@@ -63,7 +63,7 @@ const tools = computed<ToolCategory[]>(() => [
           <div>
             IT-Tools
 
-            <c-link target="_blank" rel="noopener" :href="`https://github.com/CorentinTh/it-tools/tree/v${version}`">
+            <c-link target="_blank" rel="noopener" :href="`https://github.com/dragonoasis/it-tools/tree/v${version}`">
               v{{ version }}
             </c-link>
 
@@ -73,7 +73,7 @@ const tools = computed<ToolCategory[]>(() => [
                 target="_blank"
                 rel="noopener"
                 type="primary"
-                :href="`https://github.com/CorentinTh/it-tools/tree/${commitSha}`"
+                :href="`https://github.com/dragonoasis/it-tools/tree/${commitSha}`"
               >
                 {{ commitSha }}
               </c-link>
@@ -81,7 +81,7 @@ const tools = computed<ToolCategory[]>(() => [
           </div>
           <div>
             © {{ new Date().getFullYear() }}
-            <c-link target="_blank" rel="noopener" href="https://github.com/CorentinTh">
+            <c-link target="_blank" rel="noopener" href="https://github.com/dragonoasis">
               Corentin Thomasset
             </c-link>
           </div>
@@ -123,7 +123,7 @@ const tools = computed<ToolCategory[]>(() => [
         <c-tooltip position="bottom" :tooltip="$t('home.support')">
           <c-button
             round
-            href="https://www.buymeacoffee.com/cthmsst"
+            href="https://www.buymeacoffee.com/dragonoasis"
             rel="noopener"
             target="_blank"
             class="support-button"
