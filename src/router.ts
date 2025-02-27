accb4f81eb8440b1aed7d2370db9b5457e0ee48d
dragonoasis <132386579+dragonoasis@users.noreply.github.com>
2025-02-26 23:37:53 -0800

    try to add title and description

--- src/router.ts
+++ src/router.ts
@@ -10,7 +10,13 @@ const toolsRoutes = tools.map(({ path, name, component, ...config }) => ({
   path,
   name,
   component,
-  meta: { isTool: true, layout: layouts.toolLayout, name, ...config },
+  meta: { 
+    isTool: true, 
+    layout: layouts.toolLayout, 
+    name, 
+    title: 'ASCII Text Drawer',
+    description: 'Draw ASCII art from text',
+    ...config },
 }));
 const toolsRedirectRoutes = tools
   .filter(({ redirectFrom }) => redirectFrom && redirectFrom.length > 0)
